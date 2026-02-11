d06b8c54465376c2b5ae1418c2cb4d7ee766e26a
Kirill Petrov <frozikmail@gmail.com>
2026-02-11 16:51:49 +0300

    Remove test string from the resource

--- src/app/document-viewer/document-viewer.ts
+++ src/app/document-viewer/document-viewer.ts
@@ -30,7 +30,7 @@ export class DocumentViewer implements OnDestroy {
   private readonly pageElements = viewChildren<ElementRef<HTMLDivElement>>('pageEl');
   private observer?: IntersectionObserver;
 
-  private readonly documentResource = httpResource<Document>(() => `/api/${this.id()}1.json`);
+  private readonly documentResource = httpResource<Document>(() => `/api/${this.id()}.json`);
 
   protected readonly document = computed<DocumentView | null>(() =>
     this.documentResource.hasValue() ? toDocumentView(this.documentResource.value()!) : null,
